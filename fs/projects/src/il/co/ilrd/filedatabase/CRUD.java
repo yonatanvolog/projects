8de9c0b056a00d8b59c8486c42d9b5e3eb7abd03
Yonatan.Vologdin <gasturbat@gmail.com>
2020-03-15 15:28:49 +0200

    first commit for file database

---
+++ fs/projects/src/il/co/ilrd/filedatabase/CRUD.java
@@ -0,0 +1,8 @@
+package il.co.ilrd.filedatabase;
+
+public interface CRUD <K,D> {
+	K create(D data);
+	D read (K key);
+	void update (K key, D data);
+	void delete(K key);
+}
---
+++ fs/projects/src/il/co/ilrd/filedatabase/ChangesAnalyzer.java
@@ -0,0 +1,5 @@
+package il.co.ilrd.filedatabase;
+
+public class ChangesAnalyzer {
+
+}
